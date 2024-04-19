3fa91e213215c052717261fb1290dad0ded47573
AbubakarNaveed <abubakarnaveedminhas@gmail.com>
2024-04-19 17:18:48 +0500

    some changes

--- src/app/details/page.tsx
+++ src/app/details/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Detail Page",
+  description: "This is detail page of the website",
+};
 const Details = () => {
   return <div>Details</div>;
 };
